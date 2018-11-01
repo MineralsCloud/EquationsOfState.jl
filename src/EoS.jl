8cbdd88ecd7e649b099d858ef60798c9fe7c951f
Qi Zhang <singularitti@outlook.com>
2018-11-01 02:25:35 -0400

    Implement `free_energy` & `pressure` with closures

--- src/EoS.jl
+++ src/EoS.jl
@@ -2,7 +2,7 @@ module EOS
 
 using StaticArrays
 using CurveFit
-using LsqFit
+using LsqFit: @., curve_fit
 
 export EquationOfState,
     Birch
@@ -13,18 +13,24 @@ struct Birch <: EquationOfState
     parameters::SVector{3, Float64}
 end
 
-function free_energy(eos::Birch, v::Float64, f0::Float64=0)::Float64
+function free_energy(eos::Birch)::Function
     v0, b0, bp0 = eos.parameters
-    x = (v0 / v)^(2 / 3) - 1
-    xi = 9 / 16 * b0 * v0 * x^2
-    return f0 + 2 * xi + (bp0 - 4) * xi * x
+
+    function (v::Float64, f0::Float64=0)::Float64
+        x = (v0 / v)^(2 / 3) - 1
+        xi = 9 / 16 * b0 * v0 * x^2
+        return f0 + 2 * xi + (bp0 - 4) * xi * x
+    end
 end
 
-function pressure(eos::Birch, v::Float64)::Float64
+function pressure(eos::Birch)::Function
     v0, b0, bp0 = eos.parameters
-    x = v0 / v
-    xi = x^(2 / 3) - 1
-    return 3 / 8 * b0 * x^(5 / 3) * xi * (4 + 3 * (bp0 - 4) * xi)
+
+    function (v::Float64)::Float64
+        x = v0 / v
+        xi = x^(2 / 3) - 1
+        return 3 / 8 * b0 * x^(5 / 3) * xi * (4 + 3 * (bp0 - 4) * xi)
+    end
 end
 
 end # module
