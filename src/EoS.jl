0531dbaed24076022c7ed93b078695efad147bba
Qi Zhang <singularitti@outlook.com>
2018-11-03 21:56:49 -0400

    Add `Murnaghan` EOS

--- src/EoS.jl
+++ src/EoS.jl
@@ -15,6 +15,10 @@ struct Birch <: EquationOfState
     parameters::SVector{3, Float64}
 end
 
+struct Murnaghan <: EquationOfState
+    parameters::SVector{3, Float64}
+end
+
 function fit_energy(eos::T, xdata::Vector{Float64}, ydata::Vector{Float64}, initial_parameters::Vector{Float64}; kwargs...) where {T <: EquationOfState}
     @. model(x, p) = eval_energy(T(p))(x, p[end])
     curve_fit(model, xdata, ydata, initial_parameters; kwargs...)
@@ -42,6 +46,24 @@ function eval_pressure(eos::Birch)::Function
         x = v0 / v
         xi = x^(2 / 3) - 1
         return 3 / 8 * b0 * x^(5 / 3) * xi * (4 + 3 * (bp0 - 4) * xi)
+    end
+end
+
+function eval_energy(eos::Murnaghan)::Function
+    v0, b0, bp0 = eos.parameters
+
+    function (v::Float64, f0::Float64=0)::Float64
+        x = bp0 - 1
+        y = (v0 / v) ** bp0
+        return f0 + b0 / bp0 * v * (y / x + 1) - v0 * b0 / x
+    end
+end
+
+function eval_pressure(eos::Murnaghan)::Function
+    v0, b0, bp0 = eos.parameters
+
+    function (v::Float64)::Float64
+        return b0 / bp0 * ((v0 / v) ** bp0 - 1)
     end
 end
 
