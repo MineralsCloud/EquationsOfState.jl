9af783e194561f82abaa4893b9c2f961e1ac3ffd
Qi Zhang <singularitti@outlook.com>
2018-11-04 01:47:56 -0400

    feat: Split EOS.jl into 2 modules
    `CollectionsOfEOS` and `Fitting`

---
+++ src/Fitting.jl
@@ -0,0 +1,31 @@
+"""
+# module Fitting
+
+
+
+# Examples
+
+```jldoctest
+julia>
+```
+"""
+module Fitting
+
+using LsqFit
+
+using EOS.CollectionsOfEOS
+
+export fit_energy,
+    fit_pressure
+
+function fit_energy(eos::T, xdata::Vector{Float64}, ydata::Vector{Float64}; kwargs...) where {T <: EquationOfState}
+    model(x, p) = eval_energy(T(p[1:(end - 1)])).(x, p[end])
+    curve_fit(model, xdata, ydata, [eos.parameters; minimum(ydata)]; kwargs...)
+end
+
+function fit_pressure(eos::T, xdata::Vector{Float64}, ydata::Vector{Float64}; kwargs...) where {T <: EquationOfState}
+    model(x, p) = eval_pressure(T(p)).(x)
+    curve_fit(model, xdata, ydata, eos.parameters; kwargs...)
+end
+
+end
