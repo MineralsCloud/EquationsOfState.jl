56a66ec6c944f47659eac35e966ab161b5e43e43
Qi Zhang <singularitti@outlook.com>
2018-11-04 01:18:03 -0400

    Fix 3 bugs in `fit_` functions
    1. `p` should range from index `1` to `end - 1` rather than all elements of `p`,
       since `eval_energy` needs one more parameter (`f0`) but the EOSs do not
       accept that parameter.
    2. The `model` function needs a broadcasting rather than directly apply on `x`.
    3. Deprecate `@.` in `fit_pressure`.

--- src/EoS.jl
+++ src/EoS.jl
@@ -40,12 +40,12 @@ struct Vinet <: EquationOfState
 end
 
 function fit_energy(eos::T, xdata::Vector{Float64}, ydata::Vector{Float64}; kwargs...) where {T <: EquationOfState}
-    model(x, p) = eval_energy(T(p))(x, p[end])
+    model(x, p) = eval_energy(T(p[1:(end - 1)])).(x, p[end])
     curve_fit(model, xdata, ydata, [eos.parameters; minimum(ydata)]; kwargs...)
 end
 
 function fit_pressure(eos::T, xdata::Vector{Float64}, ydata::Vector{Float64}; kwargs...) where {T <: EquationOfState}
-    @. model(x, p) = eval_pressure(T(p))(x)
+    model(x, p) = eval_pressure(T(p)).(x)
     curve_fit(model, xdata, ydata, eos.parameters; kwargs...)
 end
 
