50489602ba016c3002c20650d5b2149a408ccd48
Qi Zhang <singularitti@outlook.com>
2018-11-22 23:41:13 -0500

    Rename API
    from `finite_strain_at_order` to `finite_strain_at`

--- src/Linear.jl
+++ src/Linear.jl
@@ -11,9 +11,11 @@ julia>
 """
 module Linear
 
-function finite_strain_at_order(m::Int, reference_volume::Float64, v::Float64)
+export finite_strain_at
+
+function finite_strain_at(m::Int, reference_volume::Float64, v::Float64)
     m == 1 && return -(1 / 3 / v) * (reference_volume / v)
-    -(3 * m + 2) / (3 * v) * finite_strain_at_order(m - 1, reference_volume, v)
+    -(3 * m + 2) / (3 * v) * finite_strain_at(m - 1, reference_volume, v)
 end
 
 end
