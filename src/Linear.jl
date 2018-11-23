1e0f537883136d6fe008a177362395083792e94a
Qi Zhang <singularitti@outlook.com>
2018-11-22 22:39:29 -0500

    Deprecate the use of `Val`

--- src/Linear.jl
+++ src/Linear.jl
@@ -11,10 +11,9 @@ julia>
 """
 module Linear
 
-function finite_strain_at_order(m::Val{N}, reference_volume::Float64, v::Float64) where {N}
-    -(3 * m + 2) / (3 * v) * finite_strain_at_order(Val(m - 1), reference_volume, v)
+function finite_strain_at_order(m::Int, reference_volume::Float64, v::Float64)
+    m == 1 && return -(1 / 3 / v) * (reference_volume / v)
+    -(3 * m + 2) / (3 * v) * finite_strain_at_order(m - 1, reference_volume, v)
 end
-finite_strain_at_order(::Val{1}, reference_volume::Float64, v::Float64) = -(1 / 3 / v) * (reference_volume / v)
-finite_strain_at_order(m::Int, reference_volume::Float64, v::Float64) = finite_strain_at_order(Val(m), reference_volume, v)
 
 end
