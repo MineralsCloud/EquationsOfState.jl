54aca19a858d86f622286be6dfc58ec2b4008d4b
Qi Zhang <singularitti@outlook.com>
2018-11-24 18:07:27 -0500

    Fix a wrong formula for `finite_strain_at`

--- src/Linear.jl
+++ src/Linear.jl
@@ -13,9 +13,9 @@ module Linear
 
 export finite_strain_at
 
-function finite_strain_at(m::Int, reference_volume::Float64, v::Float64)
-    m == 1 && return -(1 / 3 / v) * (reference_volume / v)
-    -(3 * m + 2) / (3 * v) * finite_strain_at(m - 1, reference_volume, v)
+function finite_strain_at(m::Int, v0::Float64, v::Float64)
+    m == 1 && return -1 / 3 / v * (v0 / v)^(2 / 3)
+    -(3 * m + 2) / (3 * v) * finite_strain_at(m - 1, v0, v)
 end
 
 end
