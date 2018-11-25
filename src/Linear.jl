ffaec2eda123c9754ef776a7d52bd39b44d981a5
Qi Zhang <singularitti@outlook.com>
2018-11-25 00:12:28 -0500

    Deprecate dispatch of `energy_volume_derivative_at_order`
    Use `Rematch`’s `@match` macros instead, and use a closure.
    Fix a bug of `strain_derivative`.

--- src/Linear.jl
+++ src/Linear.jl
@@ -13,6 +13,7 @@ module Linear
 
 using LinearAlgebra: dot
 using Polynomials: polyder, polyfit, degree, coeffs, Poly
+using Rematch
 
 export FiniteStrain,
     EulerianStrain,
@@ -41,32 +42,31 @@ energy_strain_derivative(p::Poly, m::Int)::Poly = polyder(p, m)
 
 function strain_derivative(f::EulerianStrain, v::Float64, m::Int)::Float64
     m == 1 && return -1 / 3 / v * (f.v0 / v)^(2 / 3)
-    -(3 * m + 2) / (3 * v) * strain_derivative(f.v0, v, m - 1)
+    -(3 * m + 2) / (3 * v) * strain_derivative(f, v, m - 1)
 end
 
 function energy_volume_expansion(f::FiniteStrain, v::Float64, p::Poly, highest_order::Int=degree(p))
     # The zeroth order value plus values from the first to the ``highest_order`.
-    p(v) + dot(energy_volume_derivative_at_order(p, highest_order), compute_strain(f, v).^collect(1:highest_order))
+    p(v) + dot(energy_volume_derivatives(f, v, p, highest_order), compute_strain(f, v).^collect(1:highest_order))
 end
 
 function energy_volume_derivatives(f::FiniteStrain, v::Float64, p::Poly, highest_order::Int)
-    0 ≤ highest_order ≤ degree(p) ? (x = 1:highest_order) : throw(DomainError)
+    0 ≤ highest_order ≤ degree(p) ? (x = 1:highest_order) : throw(DomainError("The `highest_order` must be within 0 to $(degree(p))!"))
     strain_derivatives::Vector{Float64} = map(m -> strain_derivative(f, v, m), x)
     energy_derivatives::Vector{Float64} = map(m -> energy_strain_derivative(p, m), x) |> f -> f(v)
-    map(m -> energy_volume_derivative_at_order{m}(strain_derivatives, energy_derivatives), x)
+    map(m -> energy_volume_derivative_at_order(m)(strain_derivatives, energy_derivatives), x)
 end
 
-energy_volume_derivative_at_order{N}(f, e) where {N} = error("Expansion is not defined at order = $(N)!")
-energy_volume_derivative_at_order{1}(f::Vector{Float64}, e::Vector{Float64})::Float64 = e[1] * f[1]
-function energy_volume_derivative_at_order{2}(f::Vector{Float64}, e::Vector{Float64})::Float64
-    e[2] * f[1]^2 + e[1] * f[1]
-end
-function energy_volume_derivative_at_order{3}(f::Vector{Float64}, e::Vector{Float64})::Float64
-    e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
-end
-function energy_volume_derivative_at_order{4}(f::Vector{Float64}, e::Vector{Float64})::Float64
-    e[4] * f[1]^4 + 6 * f[1]^2 * f[2] * e[3] +
-    (4 * f[1] * f[3] + 3 * f[3]^2) * e[2] + e[1] * f[3]
+function energy_volume_derivative_at_order(m::Int)::Function
+    function (f::Vector{Float64}, e::Vector{Float64})
+        @match m begin
+            1 => e[1] * f[1]
+            2 => e[2] * f[1]^2 + e[1] * f[1]
+            3 => e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
+            4 => e[4] * f[1]^4 + 6 * f[1]^2 * f[2] * e[3] + (4 * f[1] * f[3] + 3 * f[3]^2) * e[2] + e[1] * f[3]
+            _ => error("Expansion is not defined at order = $(N)!")
+        end
+    end
 end
 
 end
