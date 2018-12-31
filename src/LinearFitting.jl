b15d796fecfc8a0cad0f163cb2eaae30ab10a414
Qi Zhang <singularitti@outlook.com>
2018-12-31 18:45:13 -0500

    Fix a bug of UndefVarError

--- src/LinearFitting.jl
+++ src/LinearFitting.jl
@@ -82,7 +82,7 @@ function energy_volume_derivative_at_order(m::Int)::Function
             2 => e[2] * f[1]^2 + e[1] * f[1]
             3 => e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
             4 => e[4] * f[1]^4 + 6 * f[1]^2 * f[2] * e[3] + (4 * f[1] * f[3] + 3 * f[3]^2) * e[2] + e[1] * f[3]
-            _ => error("Expansion is not defined at order = $(N)!")
+            _ => error("Expansion is not defined at order = $(m)!")
         end
     end
 end
