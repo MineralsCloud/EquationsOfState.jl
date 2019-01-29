e744ae33dce3037c1968aaab455e95fde11dd3ef
Qi Zhang <singularitti@outlook.com>
2019-01-29 12:09:55 -0500

    Rewrite NonlinearFitting.jl
    for better readability and extensibility

--- src/NonlinearFitting.jl
+++ src/NonlinearFitting.jl
@@ -12,6 +12,7 @@ julia>
 module NonlinearFitting
 
 using LsqFit
+using Transducers
 
 using EquationsOfState.Collections
 
@@ -20,18 +21,18 @@ export fit_energy,
     get_fitting_parameters
 
 function get_fitting_parameters(eos::EquationOfState)
-    Iterators.filter(x -> !(x isa NonFittingParameter), get_parameters(eos)) |> Tuple
+    collect(NotA(NonFittingParameter), get_parameters(eos))
 end
 
-function fit_energy(eos::T, xdata::S, ydata::S; kwargs...) where {T <: EquationOfState, S <: AbstractVector}
-    model(x, p) = eval_energy(T(p[1:(end - 1)])).(x, p[end])
-    curve_fit(model, xdata, ydata, [get_fitting_parameters(eos); minimum(ydata)]; kwargs...)
+function fit_energy(eos::EquationOfState, xdata::T, ydata::T; kwargs...) where {T <: AbstractVector}
+    model(x, p) = map((eval_energy ∘ typeof(eos) ∘ collect)(DropLast(1), p), (x, last(p)))
+    curve_fit(model, xdata, ydata, push!(get_fitting_parameters(eos), minimum(ydata)); kwargs...)
 end
 
-create_model(eos::EquationOfState) = (x::AbstractVector, p::AbstractVector) -> eval_pressure(T(p)).(x)
-create_model(eos::Holzapfel) = (x::AbstractVector, p::AbstractVector) -> eval_pressure(T([p; eos.z])).(x)
+create_model(eos::EquationOfState) = (x::AbstractVector, p::AbstractVector) -> map((eval_pressure ∘ typeof(eos))(p), x)
+create_model(eos::Holzapfel) = (x::AbstractVector, p::AbstractVector) -> map((eval_pressure ∘ typeof(eos))(push!(p, eos.z)), x)
 
-function fit_pressure(eos::EquationOfState, xdata::S, ydata::S; kwargs...) where {S <: AbstractVector}
+function fit_pressure(eos::EquationOfState, xdata::T, ydata::T; kwargs...) where {T <: AbstractVector}
     model = create_model(eos)
     curve_fit(model, xdata, ydata, get_fitting_parameters(eos); kwargs...)
 end
