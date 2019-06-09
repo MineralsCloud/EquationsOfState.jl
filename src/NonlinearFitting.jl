1d662c415c9b8a02ca94c63d112a2ed3009be5df
Qi Zhang <singularitti@outlook.com>
2019-06-09 15:04:35 -0400

    Use trait to rewrite NonlinearFitting.jl

--- src/NonlinearFitting.jl
+++ src/NonlinearFitting.jl
@@ -21,31 +21,27 @@ export fit_energy,
 
 convert_eltype(T::Type, a) = map(x -> convert(T, x), a)
 
-function fit_energy(eos::EquationOfState{T}, xdata::AbstractVector{T}, ydata::AbstractVector{T}; kwargs...) where {T <: AbstractFloat}
-    @. model(x, p) = eval_energy(typeof(eos)(p), x)
-    curve_fit(model, xdata, ydata, collect(eos); kwargs...)
-end
-function fit_energy(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...)
-    T = promote_type(eltype(eos), eltype(xdata), eltype(ydata), Float64)
-    fit_energy(convert_eltype(T, eos), convert_eltype(T, xdata), convert_eltype(T, ydata); kwargs...)
-end
+abstract type FitTrait end
+struct FitEnergy <: FitTrait end
+struct FitPressure <: FitTrait end
+struct FitBulkModulus <: FitTrait end
 
-function fit_pressure(eos::EquationOfState{T}, xdata::AbstractVector{T}, ydata::AbstractVector{T}; kwargs...) where {T <: AbstractFloat}
-    @. model(x, p) = eval_pressure(typeof(eos)(p), x)
-    curve_fit(model, xdata, ydata, collect(eos); kwargs...)
-end
-function fit_pressure(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...)
-    T = promote_type(eltype(eos), eltype(xdata), eltype(ydata), Float64)
-    fit_pressure(convert_eltype(T, eos), convert_eltype(T, xdata), convert_eltype(T, ydata); kwargs...)
-end
+eval_of(::FitTrait) = error("Unsupported!")
+eval_of(::FitEnergy) = eval_energy
+eval_of(::FitPressure) = eval_pressure
+eval_of(::FitBulkModulus) = eval_bulk_modulus
 
-function fit_bulk_modulus(eos::EquationOfState{T}, xdata::AbstractVector{T}, ydata::AbstractVector{T}; kwargs...) where {T <: AbstractFloat}
-    @. model(x, p) = eval_bulk_modulus(typeof(eos)(p), x)
+function lsqfit(F::FitTrait, eos::EquationOfState{T}, xdata::AbstractVector{T}, ydata::AbstractVector{T}; kwargs...) where {T <: AbstractFloat}
+    @. model(x, p) = eval_of(F)(typeof(eos)(p), x)
     curve_fit(model, xdata, ydata, collect(eos); kwargs...)
-end
-function fit_bulk_modulus(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...)
+end  # function lsqfit
+function lsqfit(F::FitTrait, eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...)
     T = promote_type(eltype(eos), eltype(xdata), eltype(ydata), Float64)
-    fit_bulk_modulus(convert_eltype(T, eos), convert_eltype(T, xdata), convert_eltype(T, ydata); kwargs...)
-end
+    lsqfit(F, convert_eltype(T, eos), convert_eltype(T, xdata), convert_eltype(T, ydata); kwargs...)
+end  # function lsqfit
+
+fit_energy(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(FitEnergy, eos, xdata, ydata; kwargs...)
+fit_pressure(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(FitPressure, eos, xdata, ydata; kwargs...)
+fit_bulk_modulus(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(FitBulkModulus, eos, xdata, ydata; kwargs...)
 
 end
