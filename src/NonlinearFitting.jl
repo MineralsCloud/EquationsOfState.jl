97ff89777b6076387e46fb6174049e92e35041b7
Qi Zhang <singularitti@outlook.com>
2019-06-14 13:30:19 -0400

    Simplify code
    `debug` is not needed to write down explicitly.

--- src/NonlinearFitting.jl
+++ src/NonlinearFitting.jl
@@ -24,9 +24,9 @@ function lsqfit(f::Function, eos::E, xdata::Vector{T}, ydata::Vector{T}; debug::
     fitted = curve_fit(model, xdata, ydata, collect(eos); kwargs...)
     debug ? fitted : E(fitted.param)
 end  # function lsqfit
-function lsqfit(f::Function, eos::E, xdata::X, ydata::Y; debug::Bool = false, kwargs...) where {E <: EquationOfState,X <: AbstractVector,Y <: AbstractVector}
+function lsqfit(f::Function, eos::E, xdata::X, ydata::Y; kwargs...) where {E <: EquationOfState,X <: AbstractVector,Y <: AbstractVector}
     T = promote_type(eltype(eos), eltype(xdata), eltype(ydata), Float64)
-    lsqfit(f, convert(similar_type(E, T), eos), convert(Vector{T}, xdata), convert(Vector{T}, ydata); debug = debug, kwargs...)
+    lsqfit(f, convert(similar_type(E, T), eos), convert(Vector{T}, xdata), convert(Vector{T}, ydata); kwargs...)
 end  # function lsqfit
 
 fit_energy(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(eval_energy, eos, xdata, ydata; kwargs...)
