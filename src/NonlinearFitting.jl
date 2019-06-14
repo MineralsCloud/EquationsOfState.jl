33a7e3c75e7e24967cc3a108301bd7925548cf6d
Qi Zhang <singularitti@outlook.com>
2019-06-14 02:24:20 -0400

    Simplify code, does not need traits

--- src/NonlinearFitting.jl
+++ src/NonlinearFitting.jl
@@ -17,34 +17,20 @@ using EquationsOfState.Collections
 
 export fit_energy,
     fit_pressure,
-    fit_bulk_modulus,
-    FitTrait,
-    FitEnergy,
-    FitPressure,
-    FitBulkModulus,
-    eval_of
+    fit_bulk_modulus
 
-abstract type FitTrait end
-struct FitEnergy <: FitTrait end
-struct FitPressure <: FitTrait end
-struct FitBulkModulus <: FitTrait end
-
-eval_of(::Type{FitEnergy}) = eval_energy
-eval_of(::Type{FitPressure}) = eval_pressure
-eval_of(::Type{FitBulkModulus}) = eval_bulk_modulus
-
-function lsqfit(F::Type{<: FitTrait}, eos::E, xdata::Vector{T}, ydata::Vector{T}; kwargs...) where {T <: AbstractFloat,E <: EquationOfState{T}}
+function lsqfit(f, eos::E, xdata::Vector{T}, ydata::Vector{T}; kwargs...) where {T <: AbstractFloat,E <: EquationOfState{T}}
     model(x, p) = eval_of(F)(E(p), x)
     fitted = curve_fit(model, xdata, ydata, collect(eos); kwargs...)
     E(fitted.param)
 end  # function lsqfit
-function lsqfit(F::Type{<: FitTrait}, eos::E, xdata::X, ydata::Y; kwargs...) where {E <: EquationOfState,X <: AbstractVector,Y <: AbstractVector}
+function lsqfit(f, eos::E, xdata::X, ydata::Y; kwargs...) where {E <: EquationOfState,X <: AbstractVector,Y <: AbstractVector}
     T = promote_type(eltype(eos), eltype(xdata), eltype(ydata), Float64)
-    lsqfit(F, convert(similar_type(E, T), eos), convert(Vector{T}, xdata), convert(Vector{T}, ydata); kwargs...)
+    lsqfit(f, convert(similar_type(E, T), eos), convert(Vector{T}, xdata), convert(Vector{T}, ydata); kwargs...)
 end  # function lsqfit
 
-fit_energy(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(FitEnergy, eos, xdata, ydata; kwargs...)
-fit_pressure(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(FitPressure, eos, xdata, ydata; kwargs...)
-fit_bulk_modulus(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(FitBulkModulus, eos, xdata, ydata; kwargs...)
+fit_energy(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(eval_energy, eos, xdata, ydata; kwargs...)
+fit_pressure(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(eval_pressure, eos, xdata, ydata; kwargs...)
+fit_bulk_modulus(eos::EquationOfState, xdata::AbstractVector, ydata::AbstractVector; kwargs...) = lsqfit(eval_bulk_modulus, eos, xdata, ydata; kwargs...)
 
 end
