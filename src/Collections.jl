320db9a92b1ddfdbf6205c0cd8c9aff58df89b82
Qi Zhang <singularitti@outlook.com>
2019-08-09 06:54:09 -0400

    Deprecate `eval_*` methods, use `calculate`
    eval_energy, eval_pressure, eval_bulk_modulus

--- src/Collections.jl
+++ src/Collections.jl
@@ -15,11 +15,11 @@ using InteractiveUtils
 using Parameters
 using StaticArrays: FieldVector, Size
 
+using EquationsOfState.Targets
+
 import StaticArrays: similar_type
 
-export eval_energy,
-    eval_pressure,
-    eval_bulk_modulus,
+export calculate,
     EquationOfState,
     FiniteStrainEquationOfState,
     PolynomialEquationOfState,
@@ -211,54 +211,54 @@ BreenanStacey(v0, b0, γ0) = BreenanStacey(v0, b0, γ0, 0)
 # ============================================================================ #
 #                               Energy evaluation                              #
 # ============================================================================ #
-eval_energy(eos::EquationOfState, vec::AbstractVector{<: Real}) = map(x->eval_energy(eos, x), vec)
-function eval_energy(eos::Birch, v::Real)
+calculate(::Type{EnergyTarget}, eos::EquationOfState, vec::AbstractVector{<: Real}) = map(x->calculate(EnergyTarget, eos, x), vec)
+function calculate(::Type{EnergyTarget}, eos::Birch, v::Real)
     @unpack v0, b0, bp0, e0 = eos
 
     x = (v0 / v)^(2 / 3) - 1
     xi = 9 / 16 * b0 * v0 * x^2
     return e0 + 2 * xi + (bp0 - 4) * xi * x
 end
-function eval_energy(eos::Murnaghan, v::Real)
+function calculate(::Type{EnergyTarget}, eos::Murnaghan, v::Real)
     @unpack v0, b0, bp0, e0 = eos
 
     x = bp0 - 1
     y = (v0 / v)^bp0
     return e0 + b0 / bp0 * v * (y / x + 1) - v0 * b0 / x
 end
-function eval_energy(eos::BirchMurnaghan2nd, v::Real)
+function calculate(::Type{EnergyTarget}, eos::BirchMurnaghan2nd, v::Real)
     @unpack v0, b0, e0 = eos
 
     f = ((v0 / v)^(2 / 3) - 1) / 2
     return e0 + 9 / 2 * b0 * v0 * f^2
 end
-function eval_energy(eos::BirchMurnaghan3rd, v::Real)
+function calculate(::Type{EnergyTarget}, eos::BirchMurnaghan3rd, v::Real)
     @unpack v0, b0, bp0, e0 = eos
 
     eta = (v0 / v)^(1 / 3)
     xi = eta^2 - 1
     return e0 + 9 / 16 * b0 * v0 * xi^2 * (6 + bp0 * xi - 4eta^2)
 end
-function eval_energy(eos::BirchMurnaghan4th, v::Real)
+function calculate(::Type{EnergyTarget}, eos::BirchMurnaghan4th, v::Real)
     @unpack v0, b0, bp0, bpp0, e0 = eos
 
     f = ((v0 / v)^(2 / 3) - 1) / 2
     h = b0 * bpp0 + bp0^2
     return e0 + 3 / 8 * v0 * b0 * f^2 * ((9h - 63bp0 + 143) * f^2 + 12(bp0 - 4) * f + 12)
 end
-function eval_energy(eos::PoirierTarantola2nd, v::Real)
+function calculate(::Type{EnergyTarget}, eos::PoirierTarantola2nd, v::Real)
     @unpack v0, b0, e0 = eos
 
     return e0 + b0 / 2 * v0 * log(v / v0)^(2 / 3)
 end
-function eval_energy(eos::PoirierTarantola3rd, v::Real)
+function calculate(::Type{EnergyTarget}, eos::PoirierTarantola3rd, v::Real)
     @unpack v0, b0, bp0, e0 = eos
 
     x = (v / v0)^(1 / 3)
     xi = -3log(x)
     return e0 + b0 / 6 * v0 * xi^2 * ((bp0 - 2) * xi + 3)
 end
-function eval_energy(eos::PoirierTarantola4th, v::Real)
+function calculate(::Type{EnergyTarget}, eos::PoirierTarantola4th, v::Real)
     @unpack v0, b0, bp0, bpp0, e0 = eos
 
     x = (v / v0)^(1 / 3)
@@ -266,14 +266,14 @@ function eval_energy(eos::PoirierTarantola4th, v::Real)
     h = b0 * bpp0 + bp0^2
     return e0 + b0 / 24v0 * xi^2 * ((h + 3bp0 + 3) * xi^2 + 4(bp0 + 2) * xi + 12)
 end
-function eval_energy(eos::Vinet, v::Real)
+function calculate(::Type{EnergyTarget}, eos::Vinet, v::Real)
     @unpack v0, b0, bp0, e0 = eos
 
     x = (v / v0)^(1 / 3)
     xi = 3 / 2 * (bp0 - 1)
     return e0 + 9b0 * v0 / xi^2 * (1 + (xi * (1 - x) - 1) * exp(xi * (1 - x)))
 end
-function eval_energy(eos::AntonSchmidt, v::Real)
+function calculate(::Type{EnergyTarget}, eos::AntonSchmidt, v::Real)
     @unpack v0, β, n, e∞ = eos
 
     x = v / v0
@@ -286,52 +286,52 @@ end
 # ============================================================================ #
 #                              Pressure evaluation                             #
 # ============================================================================ #
-eval_pressure(eos::EquationOfState, vec::AbstractVector{<: Real}) = map(x->eval_pressure(eos, x), vec)
-function eval_pressure(eos::Birch, v::Real)
+calculate(::Type{PressureTarget}, eos::EquationOfState, vec::AbstractVector{<: Real}) = map(x->calculate(PressureTarget, eos, x), vec)
+function calculate(::Type{PressureTarget}, eos::Birch, v::Real)
     @unpack v0, b0, bp0 = eos
 
     x = v0 / v
     xi = x^(2 / 3) - 1
     return 3 / 8 * b0 * x^(5 / 3) * xi * (4 + 3(bp0 - 4) * xi)
 end
-function eval_pressure(eos::Murnaghan, v::Real)
+function calculate(::Type{PressureTarget}, eos::Murnaghan, v::Real)
     @unpack v0, b0, bp0 = eos
 
     return b0 / bp0 * ((v0 / v)^bp0 - 1)
 end
-function eval_pressure(eos::BirchMurnaghan2nd, v::Real)
+function calculate(::Type{PressureTarget}, eos::BirchMurnaghan2nd, v::Real)
     @unpack v0, b0 = eos
 
     f = ((v0 / v)^(2 / 3) - 1) / 2
     return 3b0 * f * (1 + 2f)^(5 / 2)
 end
-function eval_pressure(eos::BirchMurnaghan3rd, v::Real)
+function calculate(::Type{PressureTarget}, eos::BirchMurnaghan3rd, v::Real)
     @unpack v0, b0, bp0 = eos
 
     eta = (v0 / v)^(1 / 3)
     return 3 / 2 * b0 * (eta^7 - eta^5) * (1 + 3 / 4 * (bp0 - 4) * (eta^2 - 1))
 end
-function eval_pressure(eos::BirchMurnaghan4th, v::Real)
+function calculate(::Type{PressureTarget}, eos::BirchMurnaghan4th, v::Real)
     @unpack v0, b0, bp0, bpp0 = eos
 
     f = ((v0 / v)^(2 / 3) - 1) / 2
     h = b0 * bpp0 + bp0^2
     return b0 / 2 * (2f + 1)^(5 / 2) * ((9h - 63bp0 + 143) * f^2 + 9(bp0 - 4) * f + 6)
 end
-function eval_pressure(eos::PoirierTarantola2nd, v::Real)
+function calculate(::Type{PressureTarget}, eos::PoirierTarantola2nd, v::Real)
     @unpack v0, b0 = eos
 
     x = (v / v0)^(1 / 3)
     return -b0 / x * log(x)
 end
-function eval_pressure(eos::PoirierTarantola3rd, v::Real)
+function calculate(::Type{PressureTarget}, eos::PoirierTarantola3rd, v::Real)
     @unpack v0, b0, bp0 = eos
 
     x = v / v0
     xi = log(x)
     return -b0 * xi / 2x * ((bp0 - 2) * xi - 2)
 end
-function eval_pressure(eos::PoirierTarantola4th, v::Real)
+function calculate(::Type{PressureTarget}, eos::PoirierTarantola4th, v::Real)
     @unpack v0, b0, bp0, bpp0 = eos
 
     x = (v / v0)^(1 / 3)
@@ -339,20 +339,20 @@ function eval_pressure(eos::PoirierTarantola4th, v::Real)
     h = b0 * bpp0 + bp0^2
     return -b0 * xi / 6 / x * ((h + 3bp0 + 3) * xi^2 + 3(bp0 + 6) * xi + 6)
 end
-function eval_pressure(eos::Vinet, v::Real)
+function calculate(::Type{PressureTarget}, eos::Vinet, v::Real)
     @unpack v0, b0, bp0 = eos
 
     x = (v / v0)^(1 / 3)
     xi = 3 / 2 * (bp0 - 1)
     return 3b0 / x^2 * (1 - x) * exp(xi * (1 - x))
 end
-function eval_pressure(eos::AntonSchmidt, v::Real)
+function calculate(::Type{PressureTarget}, eos::AntonSchmidt, v::Real)
     @unpack v0, β, n = eos
 
     x = v / v0
     return -β * x^n * log(x)
 end
-function eval_pressure(eos::BreenanStacey, v::Real)
+function calculate(::Type{PressureTarget}, eos::BreenanStacey, v::Real)
     @unpack v0, b0, γ0 = eos
 
     x = v0 / v
@@ -364,40 +364,40 @@ end
 # ============================================================================ #
 #                            Bulk modulus evaluation                           #
 # ============================================================================ #
-eval_bulk_modulus(eos::EquationOfState, vec::AbstractVector{<: Real}) = map(x->eval_bulk_modulus(eos, x), vec)
-function eval_bulk_modulus(eos::BirchMurnaghan2nd, v::Real)
+calculate(::Type{BulkModulusTarget}, eos::EquationOfState, vec::AbstractVector{<: Real}) = map(x->calculate(BulkModulusTarget, eos, x), vec)
+function calculate(::Type{BulkModulusTarget}, eos::BirchMurnaghan2nd, v::Real)
     @unpack v0, b0 = eos
 
     f = ((v0 / v)^(2 / 3) - 1) / 2
     return b0 * (7f + 1) * (2f + 1)^(5 / 2)
 end
-function eval_bulk_modulus(eos::BirchMurnaghan3rd, v::Real)
+function calculate(::Type{BulkModulusTarget}, eos::BirchMurnaghan3rd, v::Real)
     @unpack v0, b0, bp0 = eos
 
     f = ((v0 / v)^(2 / 3) - 1) / 2
     return b0 / 2 * (2f + 1)^(5 / 2) * ((27f^2 + 6f) * (bp0 - 4) - 4f + 2)
 end
-function eval_bulk_modulus(eos::BirchMurnaghan4th, v::Real)
+function calculate(::Type{BulkModulusTarget}, eos::BirchMurnaghan4th, v::Real)
     @unpack v0, b0, bp0, bpp0 = eos
 
     f = ((v0 / v)^(2 / 3) - 1) / 2
     h = b0 * bpp0 + bp0^2
     return b0 / 6 * (2f + 1)^(5 / 2) * ((99h - 693bp0 + 1573) * f^3 + (27h - 108bp0 + 105) * f^2 + 6f * (3bp0 - 5) + 6)
 end
-function eval_bulk_modulus(eos::PoirierTarantola2nd, v::Real)
+function calculate(::Type{BulkModulusTarget}, eos::PoirierTarantola2nd, v::Real)
     @unpack v0, b0 = eos
 
     x = (v / v0)^(1 / 3)
     return b0 / x * (1 - log(x))
 end
-function eval_bulk_modulus(eos::PoirierTarantola3rd, v::Real)
+function calculate(::Type{BulkModulusTarget}, eos::PoirierTarantola3rd, v::Real)
     @unpack v0, b0, bp0 = eos
 
     x = v / v0
     xi = log(x)
     return -b0 / 2x * (((bp0 - 2) * xi + 2 - 2bp0) * xi + 2)
 end
-function eval_bulk_modulus(eos::PoirierTarantola4th, v::Real)
+function calculate(::Type{BulkModulusTarget}, eos::PoirierTarantola4th, v::Real)
     @unpack v0, b0, bp0, bpp0 = eos
 
     x = (v / v0)^(1 / 3)
@@ -405,14 +405,14 @@ function eval_bulk_modulus(eos::PoirierTarantola4th, v::Real)
     h = b0 * bpp0 + bp0^2
     return -b0 / (6x) * ((h + 3bp0 + 3) * xi^3 - 3xi^2 * (h + 2bp0 + 1) - 6xi * (bp0 + 1) - 6)
 end
-function eval_bulk_modulus(eos::Vinet, v::Real)
+function calculate(::Type{BulkModulusTarget}, eos::Vinet, v::Real)
     @unpack v0, b0, bp0 = eos
 
     x = (v / v0)^(1 / 3)
     xi = 3 / 2 * (bp0 - 1)
     return -b0 / (2x^2) * (3x * (x - 1) * (bp0 - 1) + 2(x - 2)) * exp(-xi * (x - 1))
 end
-function eval_bulk_modulus(eos::AntonSchmidt, v::Real)
+function calculate(::Type{BulkModulusTarget}, eos::AntonSchmidt, v::Real)
     @unpack v0, β, n = eos
 
     x = v / v0
