ed57bce1f2b0c89f0a4709e60bb9517e264c7253
Qi Zhang <singularitti@outlook.com>
2019-08-31 02:45:28 -0400

    Change `^(1 / 3)` to `cbrt` in src/FiniteStrains.jl

--- src/FiniteStrains.jl
+++ src/FiniteStrains.jl
@@ -20,9 +20,9 @@ struct LagrangianStrain <: FiniteStrain end
 struct NaturalStrain <: FiniteStrain end
 struct InfinitesimalStrain <: FiniteStrain end
 
-getstrain(::EulerianStrain, v0::Real, v::Real) = ((v0 / v)^(2 / 3) - 1) / 2
-getstrain(::LagrangianStrain, v0::Real, v::Real) = ((v / v0)^(2 / 3) - 1) / 2
+getstrain(::EulerianStrain, v0::Real, v::Real) = (cbrt(v0 / v)^2 - 1) / 2
+getstrain(::LagrangianStrain, v0::Real, v::Real) = (cbrt(v / v0)^2 - 1) / 2
 getstrain(::NaturalStrain, v0::Real, v::Real) = log(v / v0) / 3
-getstrain(::InfinitesimalStrain, v0::Real, v::Real) = 1 - (v0 / v)^(1 / 3)
+getstrain(::InfinitesimalStrain, v0::Real, v::Real) = 1 - cbrt(v0 / v)
 
 end
