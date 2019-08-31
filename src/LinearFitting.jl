9d5347584ef845cb616d2f025ea0cf7315b63d88
Qi Zhang <singularitti@outlook.com>
2019-08-31 02:59:35 -0400

    Rename variable

--- src/LinearFitting.jl
+++ src/LinearFitting.jl
@@ -28,31 +28,31 @@ energy_strain_expansion(f::Vector{<:Real}, e::Vector{<:Real}, n::Int)::Poly = po
 
 energy_strain_derivative(p::Poly, m::Int)::Poly = polyder(p, m)
 
-function strain_volume_derivative(T::EulerianStrain, v0::Real, v::Real, m::Int)
+function strain_volume_derivative(s::EulerianStrain, v0::Real, v::Real, m::Int)
     m == 1 && return -1 / (3v) * cbrt(v0 / v)^2
-    -(3m + 2) / (3v) * strain_volume_derivative(T, v0, v, m - 1)
+    -(3m + 2) / (3v) * strain_volume_derivative(s, v0, v, m - 1)
 end
-function strain_volume_derivative(T::LagrangianStrain, v0::Real, v::Real, m::Int)
+function strain_volume_derivative(s::LagrangianStrain, v0::Real, v::Real, m::Int)
     m == 1 && return -1 / (3v) * cbrt(v / v0)^2
-    -(3m - 2) / (3v) * strain_volume_derivative(T, v0, v, m - 1)
+    -(3m - 2) / (3v) * strain_volume_derivative(s, v0, v, m - 1)
 end
-function strain_volume_derivative(T::NaturalStrain, v0::Real, v::Real, m::Int)
+function strain_volume_derivative(s::NaturalStrain, v0::Real, v::Real, m::Int)
     m == 1 && return 1 / (3v)
-    -m / v * strain_volume_derivative(T, v0, v, m - 1)
+    -m / v * strain_volume_derivative(s, v0, v, m - 1)
 end
-function strain_volume_derivative(T::InfinitesimalStrain, v0::Real, v::Real, m::Int)
-    m == 1 && return (1 - getstrain(T, v0, v))^4 / 3 / v0
-    -(3m + 1) / (3v) * strain_volume_derivative(T, v0, v, m - 1)
+function strain_volume_derivative(s::InfinitesimalStrain, v0::Real, v::Real, m::Int)
+    m == 1 && return (1 - getstrain(s, v0, v))^4 / 3 / v0
+    -(3m + 1) / (3v) * strain_volume_derivative(s, v0, v, m - 1)
 end
 
-function energy_volume_expansion(T::FiniteStrain, v0::Real, v::Real, p::Poly, highest_order::Int = degree(p))
+function energy_volume_expansion(s::FiniteStrain, v0::Real, v::Real, p::Poly, highest_order::Int = degree(p))
     # The zeroth order value plus values from the first to the ``highest_order`.
-    p(v) + dot(energy_volume_derivatives(T, v0, v, p, highest_order), getstrain(T, v0, v).^collect(1:highest_order))
+    p(v) + dot(energy_volume_derivatives(s, v0, v, p, highest_order), getstrain(s, v0, v).^collect(1:highest_order))
 end
 
-function energy_volume_derivatives(T::FiniteStrain, v0::Real, v::Real, p::Poly, highest_order::Int)
+function energy_volume_derivatives(s::FiniteStrain, v0::Real, v::Real, p::Poly, highest_order::Int)
     0 ≤ highest_order ≤ degree(p) ? (x = 1:highest_order) : throw(DomainError("The `highest_order` must be within 0 to $(degree(p))!"))
-    strain_derivatives = map(m -> strain_volume_derivative(T, v0, v, m), x)
+    strain_derivatives = map(m -> strain_volume_derivative(s, v0, v, m), x)
     energy_derivatives = map(f -> f(v), map(m -> energy_strain_derivative(p, m), x))
     map(m -> energy_volume_derivative_at_order(m)(strain_derivatives, energy_derivatives), x)
 end
