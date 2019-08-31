73f60febf3fde2f73e248cb65f39d480fdfd07ec
Qi Zhang <singularitti@outlook.com>
2019-08-30 23:04:25 -0400

    Use `EquationOfStateForm` singleton instances to dispatch `find_volume`

--- src/FindVolume.jl
+++ src/FindVolume.jl
@@ -18,8 +18,8 @@ using EquationsOfState.Collections
 
 export find_volume
 
-function find_volume(T::Type{<:EquationOfStateForm}, eos::EquationOfState, y::Real, interval, method)
-    f = v -> apply(T, eos, v) - y
+function find_volume(form::EquationOfStateForm, eos::EquationOfState, y::Real, interval, method)
+    f = v -> apply(form, eos, v) - y
     solutions = roots(f, interval, method)
     length(solutions) != 1 ? error("Multiple roots find!") : return first(solutions)
 end # function find_volume
