8619c665f943c0cf20f2cee9f7c571287f3f13df
Qi Zhang <singularitti@outlook.com>
2019-09-13 04:49:57 -0400

    Rewrite `findvolume` in 3 methods
    1. AbstractBracketing
    2. AbstractNonBracketing
    3. Adaptive (try all)

--- src/FindVolume.jl
+++ src/FindVolume.jl
@@ -11,16 +11,67 @@ julia>
 """
 module FindVolume
 
-using Roots: find_zero
+using Statistics: median
+
+using Roots: find_zero,
+             AbstractBracketing,
+             AbstractNonBracketing,
+             AbstractHalleyLikeMethod,
+             AbstractNewtonLikeMethod,
+             AbstractAlefeldPotraShi,
+             AbstractBisection,
+             AbstractSecant,
+             Brent,
+             Newton,
+             ConvergenceFailed
 
 using EquationsOfState
 using EquationsOfState.Collections
 
 export findvolume
 
-function findvolume(form::EquationOfStateForm, eos::EquationOfState, y::Real, closepoint::Real, method)
+function findvolume(
+    form::EquationOfStateForm,
+    eos::EquationOfState,
+    y::Real,
+    domain::Union{AbstractVector,Tuple},
+    method::AbstractBracketing
+)
     f(v) = apply(form, eos, v) - y
-    return find_zero(f, closepoint, method)
+    return find_zero(f, (minimum(domain), maximum(domain)), method)
+end # function findvolume
+function findvolume(
+    form::EquationOfStateForm,
+    eos::EquationOfState,
+    y::Real,
+    domain::Union{AbstractVector,Tuple},
+    method::Union{AbstractNonBracketing,AbstractHalleyLikeMethod,AbstractNewtonLikeMethod}
+)
+    f(v) = apply(form, eos, v) - y
+    return find_zero(f, median(domain), method)
+end # function findvolume
+function findvolume(
+    form::EquationOfStateForm,
+    eos::EquationOfState,
+    y::Real,
+    domain::Union{AbstractVector,Tuple}
+)
+    for T in [
+        subtypes(AbstractAlefeldPotraShi);
+        subtypes(AbstractBisection);
+        Brent;
+        subtypes(AbstractHalleyLikeMethod);
+        Newton;
+        subtypes(AbstractSecant)
+    ]
+        try
+            findvolume(form, eos, y, domain, T())
+        catch
+            BoundsError, ConvergenceFailed
+            @info("Method \"$T\" failed!")
+            continue
+        end
+    end
 end # function findvolume
 
 end
