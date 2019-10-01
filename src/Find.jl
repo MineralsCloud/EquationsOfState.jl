25b39d7d5f84b62fc2e87cbeb4a98a6aca580d5f
Qi Zhang <singularitti@outlook.com>
2019-10-01 02:20:49 -0400

    Implement `Statistics.middle` for `AbstractQuantity`
    to let `median` works

--- src/Find.jl
+++ src/Find.jl
@@ -12,8 +12,8 @@ julia>
 module Find
 
 using InteractiveUtils: subtypes
-using Statistics: median
-using Unitful: AbstractQuantity, ustrip
+import Statistics
+using Unitful: AbstractQuantity, ustrip, upreferred
 
 using Roots: find_zero,
              AbstractBracketing,
@@ -60,7 +60,7 @@ function _adapt_domain(
         AbstractNewtonLikeMethod,
     },
 )
-    return median(domain)
+    return Statistics.median(domain)
 end # function _adapt_domain
 
 function findvolume(
@@ -96,5 +96,15 @@ function findvolume(
         end
     end
 end # function findvolume
+
+Statistics.middle(x::AbstractQuantity) = (x + zero(x)) / 1
+Statistics.middle(a::T, b::T) where {T<:AbstractQuantity} = middle(ustrip(a), ustrip(b)) * unit(a)
+function Statistics.middle(a::AbstractQuantity, b::AbstractQuantity)
+    @assert(dimension(a) == dimension(b))
+    a0, b0 = promote(map(ustrip, (a, b))...)
+    a, b = a0 * unit(a), b0 * unit(b)
+    a, b = map(upreferred, a, b)
+    return middle(a, b)
+end # Statistics.middle
 
 end
