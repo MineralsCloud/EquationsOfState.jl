8635c0d436aa6018345b1178c6b18e9dc7d0c42d
Qi Zhang <singularitti@outlook.com>
2019-10-01 04:51:18 -0400

    style: Reformat code

--- src/Find.jl
+++ src/Find.jl
@@ -32,39 +32,20 @@ using ..Collections: EquationOfState, apply
 
 export findvolume
 
-function _whose_zero(
-    form::EquationForm,
-    eos::EquationOfState,
-    y::AbstractQuantity,
-)
+function _whose_zero(form::EquationForm, eos::EquationOfState, y::AbstractQuantity)
     @assert(eltype(eos) <: AbstractQuantity, "The elements type mismatched!")
     return v::AbstractQuantity -> ustrip(apply(form, eos, v) - y)
 end # function _whose_zero
-function _whose_zero(
-    form::EquationForm,
-    eos::EquationOfState,
-    y::Real,
-)
+function _whose_zero(form::EquationForm, eos::EquationOfState, y::Real)
     @assert(eltype(eos) <: Real, "The elements type mismatched!")
     return v::Real -> apply(form, eos, v) - y
 end # function _whose_zero
 
-function findvolume(
-    form::EquationForm,
-    eos::EquationOfState,
-    y,
-    x0,
-    method,
-)
+function findvolume(form::EquationForm, eos::EquationOfState, y, x0, method)
     f = _whose_zero(form, eos, y)
     return find_zero(f, x0, method)
 end # function findvolume
-function findvolume(
-    form::EquationForm,
-    eos::EquationOfState,
-    y,
-    x0,
-)
+function findvolume(form::EquationForm, eos::EquationOfState, y, x0)
     for T in [
         subtypes(AbstractAlefeldPotraShi)
         subtypes(AbstractBisection)
@@ -84,7 +65,8 @@ function findvolume(
 end # function findvolume
 
 Statistics.middle(x::AbstractQuantity) = (x + zero(x)) / 1
-Statistics.middle(a::T, b::T) where {T<:AbstractQuantity} = middle(ustrip(a), ustrip(b)) * unit(a)
+Statistics.middle(a::T, b::T) where {T<:AbstractQuantity} =
+    middle(ustrip(a), ustrip(b)) * unit(a)
 function Statistics.middle(a::AbstractQuantity, b::AbstractQuantity)
     @assert(dimension(a) == dimension(b))
     a0, b0 = promote(map(ustrip, (a, b))...)
