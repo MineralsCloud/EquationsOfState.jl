6ba985450e2bb6a280ba184d5204fba0d0f5330b
Qi Zhang <singularitti@outlook.com>
2019-10-03 04:56:39 -0400

    Deprecate `_whose_zero` in src/Find.jl
    It is not necessary to distinguish `AbstractQuantity` or
    `Real` for `v`. `find_zero` is duck-typing.

--- src/Find.jl
+++ src/Find.jl
@@ -31,17 +31,8 @@ using ..Collections: EquationOfState, apply
 
 export findvolume
 
-function _whose_zero(form::EquationForm, eos::EquationOfState, y::AbstractQuantity)
-    @assert(eltype(eos) <: AbstractQuantity, "The elements type mismatched!")
-    return v::AbstractQuantity -> ustrip(apply(form, eos, v) - y)
-end # function _whose_zero
-function _whose_zero(form::EquationForm, eos::EquationOfState, y::Real)
-    @assert(eltype(eos) <: Real, "The elements type mismatched!")
-    return v::Real -> apply(form, eos, v) - y
-end # function _whose_zero
-
 function findvolume(form::EquationForm, eos::EquationOfState, y, x0, method)
-    f = _whose_zero(form, eos, y)
+    f = v -> apply(form, eos, v) - y
     return find_zero(f, x0, method)
 end # function findvolume
 function findvolume(form::EquationForm, eos::EquationOfState, y, x0::Union{AbstractVector,Tuple})
