b0a91a209216adb3af41ac4afb01cf63c37136b8
Qi Zhang <singularitti@outlook.com>
2019-11-25 23:51:47 -0500

    :fire: Deprecate prelude.jl move code to EquationsOfState.jl

--- src/EquationsOfState.jl
+++ src/EquationsOfState.jl
@@ -1,6 +1,12 @@
 module EquationsOfState
 
-include("prelude.jl")
+export EquationForm, EnergyForm, PressureForm, BulkModulusForm
+
+abstract type EquationForm end
+struct EnergyForm <: EquationForm end
+struct PressureForm <: EquationForm end
+struct BulkModulusForm <: EquationForm end
+
 include("Collections.jl")
 include("NonlinearFitting.jl")
 include("FiniteStrains.jl")
