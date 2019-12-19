MineralsCloud/EquationsOfState.jl

commit 43592c36a042b0dd4817aa43cf48dbc927fee7d2

Change 1 `using` to `import`

src/Collections.jl
@@ -5,10 +5,11 @@ a (an) volume (array of volumes).
 """
 module Collections
 
-using Unitful
 using Unitful: AbstractQuantity
 
 using EquationsOfState: EnergyForm, PressureForm, BulkModulusForm
+
+import Unitful
 
 export apply,
     EquationOfState,

commit 61b9b4ff0da5c63b7c66a4a84bdbc9ee2a898834

:bug: Fix importing Unitful.jl
caused by 43592c36a042b0dd4817aa43cf48dbc927fee7d2

src/Collections.jl
@@ -5,7 +5,7 @@ a (an) volume (array of volumes).
 """
 module Collections
 
-using Unitful: AbstractQuantity
+using Unitful: AbstractQuantity, dimension, @u_str
 
 using EquationsOfState: EnergyForm, PressureForm, BulkModulusForm
 