b7d883b797b3863b6099d9a14e11540563392130
Qi Zhang <singularitti@outlook.com>
2020-04-12 22:27:49 -0400

    Deprecate `Rematch: @match`, use `if`

--- src/LinearFitting.jl
+++ src/LinearFitting.jl
@@ -5,7 +5,6 @@ module LinearFitting
 
 using LinearAlgebra: dot
 using Polynomials: polyder, polyfit, degree, coeffs, Poly
-using Rematch: @match
 
 using ..FiniteStrains
 
@@ -71,16 +70,19 @@ end
 
 function energy_volume_derivative_at_order(m::Int)::Function
     function (f::Vector{<:Real}, e::Vector{<:Real})
-        @match m begin
-            1 => e[1] * f[1]
-            2 => e[2] * f[1]^2 + e[1] * f[1]
-            3 => e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
-            4 =>
-                e[4] * f[1]^4 +
-                6 * f[1]^2 * f[2] * e[3] +
-                (4 * f[1] * f[3] + 3 * f[3]^2) * e[2] +
-                e[1] * f[3]
-            _ => error("Expansion is not defined at order = $(m)!")
+        if m == 1
+            e[1] * f[1]
+        elseif m == 2
+            e[2] * f[1]^2 + e[1] * f[1]
+        elseif m == 3
+            e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
+        elseif m == 4
+            e[4] * f[1]^4 +
+            6 * f[1]^2 * f[2] * e[3] +
+            (4 * f[1] * f[3] + 3 * f[3]^2) * e[2] +
+            e[1] * f[3]
+        else
+            error("Expansion is not defined at order = $(m)!")
         end
     end
 end
