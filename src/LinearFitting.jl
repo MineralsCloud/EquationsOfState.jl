4700d80f20a106239fbfaa54d18af5c8d98a2b83
singularitti <singularitti@outlook.com>
2020-05-06 07:47:01 -0400

    Restyle code
    using Yin Wang's style

--- src/LinearFitting.jl
+++ src/LinearFitting.jl
@@ -32,25 +32,37 @@ energy_strain_expansion(f::Vector{<:Real}, e::Vector{<:Real}, n::Int) = polyfit(
 energy_strain_derivative(p::Poly, deg) = polyder(p, deg)
 
 function strain_volume_derivative(s::EulerianStrain, v0, v, deg)
-    deg == 1 && return -1 / (3v) * cbrt(v0 / v)^2
-    -(3deg + 2) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
+    if deg == 1
+        return -1 / (3v) * cbrt(v0 / v)^2
+    else  # Recursion
+        return -(3deg + 2) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
+    end
 end
 function strain_volume_derivative(s::LagrangianStrain, v0, v, deg)
-    deg == 1 && return -1 / (3v) * cbrt(v / v0)^2
-    -(3deg - 2) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
+    if deg == 1
+        return -1 / (3v) * cbrt(v / v0)^2
+    else  # Recursion
+        return -(3deg - 2) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
+    end
 end
 function strain_volume_derivative(s::NaturalStrain, v0, v, deg)
-    deg == 1 && return 1 / (3v)
-    -deg / v * strain_volume_derivative(s, v0, v, deg - 1)
+    if deg == 1
+        return 1 / (3v)
+    else  # Recursion
+        return -deg / v * strain_volume_derivative(s, v0, v, deg - 1)
+    end
 end
 function strain_volume_derivative(s::InfinitesimalStrain, v0, v, deg)
-    deg == 1 && return (1 - s(v0, v))^4 / 3 / v0
-    -(3deg + 1) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
+    if deg == 1
+        return (1 - s(v0, v))^4 / 3 / v0
+    else  # Recursion
+        return -(3deg + 1) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
+    end
 end
 
 function energy_volume_expansion(s::FiniteStrain, v0, v, p::Poly, highest_order = degree(p))
     # The zeroth order value plus values from the first to the ``highest_order`.
-    p(v) + dot(
+    return p(v) + dot(
         energy_volume_derivatives(s, v0, v, p, highest_order),
         s(v0, v) .^ collect(1:highest_order),
     )
@@ -61,7 +73,7 @@ function energy_volume_derivatives(s::FiniteStrain, v0, v, p::Poly, highest_orde
     throw(DomainError("The `highest_order` must be within 0 to $(degree(p))!"))
     strain_derivatives = map(m -> strain_volume_derivative(s, v0, v, m), x)
     energy_derivatives = map(f -> f(v), map(m -> energy_strain_derivative(p, m), x))
-    map(
+    return map(
         m -> energy_volume_derivative_at_order(m)(strain_derivatives, energy_derivatives),
         x,
     )
@@ -70,13 +82,13 @@ end
 function energy_volume_derivative_at_order(deg)
     function (f::Vector{<:Real}, e::Vector{<:Real})
         if deg == 1
-            e[1] * f[1]
+            return e[1] * f[1]
         elseif deg == 2
-            e[2] * f[1]^2 + e[1] * f[1]
+            return e[2] * f[1]^2 + e[1] * f[1]
         elseif deg == 3
-            e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
+            return e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
         elseif deg == 4
-            e[4] * f[1]^4 +
+            return e[4] * f[1]^4 +
             6 * f[1]^2 * f[2] * e[3] +
             (4 * f[1] * f[3] + 3 * f[3]^2) * e[2] +
             e[1] * f[3]
