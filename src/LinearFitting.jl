af1daa33d40896f4a24f43bca1a7a02c23264d0b
singularitti <singularitti@outlook.com>
2020-05-06 07:17:40 -0400

    Merge src/FiniteStrains.jl to src/LinearFitting.jl

--- src/LinearFitting.jl
+++ src/LinearFitting.jl
@@ -7,14 +7,26 @@ using LinearAlgebra: dot
 using Polynomials: polyder, degree, coeffs, Poly
 using Polynomials.PolyCompat: polyfit
 
-using ..FiniteStrains
-
+export FiniteStrain,
+    EulerianStrain, LagrangianStrain, NaturalStrain, InfinitesimalStrain, getstrain
 export energy_strain_expansion,
     energy_strain_derivative,
     strain_volume_derivative,
     energy_volume_expansion,
     energy_volume_derivatives,
     energy_volume_derivative_at_order
+
+abstract type FiniteStrain end
+
+struct EulerianStrain <: FiniteStrain end
+struct LagrangianStrain <: FiniteStrain end
+struct NaturalStrain <: FiniteStrain end
+struct InfinitesimalStrain <: FiniteStrain end
+
+getstrain(::EulerianStrain, v0::Real, v::Real) = (cbrt(v0 / v)^2 - 1) / 2
+getstrain(::LagrangianStrain, v0::Real, v::Real) = (cbrt(v / v0)^2 - 1) / 2
+getstrain(::NaturalStrain, v0::Real, v::Real) = log(v / v0) / 3
+getstrain(::InfinitesimalStrain, v0::Real, v::Real) = 1 - cbrt(v0 / v)
 
 energy_strain_expansion(f::Vector{<:Real}, e::Vector{<:Real}, n::Int)::Poly =
     polyfit(f, e, n)
