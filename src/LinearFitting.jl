d874630c418059831ec8af60accb085eb4d3310f
singularitti <singularitti@outlook.com>
2020-05-06 07:32:29 -0400

    Relax type constraints
    as suggested in
    https://white.ucc.asn.au/2020/04/19/Julia-Antipatterns.html

--- src/LinearFitting.jl
+++ src/LinearFitting.jl
@@ -4,8 +4,8 @@ This module provides some linear fitting methods.
 module LinearFitting
 
 using LinearAlgebra: dot
-using Polynomials: polyder, degree, coeffs, Poly
-using Polynomials.PolyCompat: polyfit
+using Polynomials: degree, coeffs
+using Polynomials.PolyCompat: polyfit, polyder, Poly
 
 export FiniteStrain, EulerianStrain, LagrangianStrain, NaturalStrain, InfinitesimalStrain
 export energy_strain_expansion,
@@ -27,35 +27,28 @@ struct InfinitesimalStrain <: FiniteStrain end
 (::NaturalStrain)(v0, v) = log(v / v0) / 3
 (::InfinitesimalStrain)(v0, v) = 1 - cbrt(v0 / v)
 
-energy_strain_expansion(f::Vector{<:Real}, e::Vector{<:Real}, n::Int)::Poly =
-    polyfit(f, e, n)
+energy_strain_expansion(f::Vector{<:Real}, e::Vector{<:Real}, n::Int) = polyfit(f, e, n)
 
-energy_strain_derivative(p::Poly, m::Int)::Poly = polyder(p, m)
+energy_strain_derivative(p::Poly, deg) = polyder(p, deg)
 
-function strain_volume_derivative(s::EulerianStrain, v0::Real, v::Real, m::Int)
-    m == 1 && return -1 / (3v) * cbrt(v0 / v)^2
-    -(3m + 2) / (3v) * strain_volume_derivative(s, v0, v, m - 1)
+function strain_volume_derivative(s::EulerianStrain, v0, v, deg)
+    deg == 1 && return -1 / (3v) * cbrt(v0 / v)^2
+    -(3deg + 2) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
 end
-function strain_volume_derivative(s::LagrangianStrain, v0::Real, v::Real, m::Int)
-    m == 1 && return -1 / (3v) * cbrt(v / v0)^2
-    -(3m - 2) / (3v) * strain_volume_derivative(s, v0, v, m - 1)
+function strain_volume_derivative(s::LagrangianStrain, v0, v, deg)
+    deg == 1 && return -1 / (3v) * cbrt(v / v0)^2
+    -(3deg - 2) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
 end
-function strain_volume_derivative(s::NaturalStrain, v0::Real, v::Real, m::Int)
-    m == 1 && return 1 / (3v)
-    -m / v * strain_volume_derivative(s, v0, v, m - 1)
+function strain_volume_derivative(s::NaturalStrain, v0, v, deg)
+    deg == 1 && return 1 / (3v)
+    -deg / v * strain_volume_derivative(s, v0, v, deg - 1)
 end
-function strain_volume_derivative(s::InfinitesimalStrain, v0::Real, v::Real, m::Int)
-    m == 1 && return (1 - s(v0, v))^4 / 3 / v0
-    -(3m + 1) / (3v) * strain_volume_derivative(s, v0, v, m - 1)
+function strain_volume_derivative(s::InfinitesimalStrain, v0, v, deg)
+    deg == 1 && return (1 - s(v0, v))^4 / 3 / v0
+    -(3deg + 1) / (3v) * strain_volume_derivative(s, v0, v, deg - 1)
 end
 
-function energy_volume_expansion(
-    s::FiniteStrain,
-    v0::Real,
-    v::Real,
-    p::Poly,
-    highest_order::Int = degree(p),
-)
+function energy_volume_expansion(s::FiniteStrain, v0, v, p::Poly, highest_order = degree(p))
     # The zeroth order value plus values from the first to the ``highest_order`.
     p(v) + dot(
         energy_volume_derivatives(s, v0, v, p, highest_order),
@@ -63,13 +56,7 @@ function energy_volume_expansion(
     )
 end
 
-function energy_volume_derivatives(
-    s::FiniteStrain,
-    v0::Real,
-    v::Real,
-    p::Poly,
-    highest_order::Int,
-)
+function energy_volume_derivatives(s::FiniteStrain, v0, v, p::Poly, highest_order)
     0 ≤ highest_order ≤ degree(p) ? (x = 1:highest_order) :
     throw(DomainError("The `highest_order` must be within 0 to $(degree(p))!"))
     strain_derivatives = map(m -> strain_volume_derivative(s, v0, v, m), x)
@@ -80,21 +67,21 @@ function energy_volume_derivatives(
     )
 end
 
-function energy_volume_derivative_at_order(m::Int)::Function
+function energy_volume_derivative_at_order(deg)
     function (f::Vector{<:Real}, e::Vector{<:Real})
-        if m == 1
+        if deg == 1
             e[1] * f[1]
-        elseif m == 2
+        elseif deg == 2
             e[2] * f[1]^2 + e[1] * f[1]
-        elseif m == 3
+        elseif deg == 3
             e[3] * f[1]^3 + 3 * f[1] * f[2] * e[2] + e[1] * f[3]
-        elseif m == 4
+        elseif deg == 4
             e[4] * f[1]^4 +
             6 * f[1]^2 * f[2] * e[3] +
             (4 * f[1] * f[3] + 3 * f[3]^2) * e[2] +
             e[1] * f[3]
         else
-            error("Expansion is not defined at order = $(m)!")
+            error("Expansion is not defined at order = $(deg)!")
         end
     end
 end
