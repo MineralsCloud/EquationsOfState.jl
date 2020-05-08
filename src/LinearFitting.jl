489964c737149a379cef5d658d1b3a2bfae2890f
singularitti <singularitti@outlook.com>
2020-05-08 04:31:31 -0400

    Deprecate `FiniteStrain` types

--- src/LinearFitting.jl
+++ src/LinearFitting.jl
@@ -7,29 +7,7 @@ using Polynomials: Polynomial, fit, derivative, roots, coeffs
 
 using ..Collections: PolynomialEOS
 
-export FiniteStrain, Eulerian, Lagrangian, Natural, Infinitesimal, linearfit
-
-abstract type FiniteStrain end
-struct Eulerian <: FiniteStrain end
-struct Lagrangian <: FiniteStrain end
-struct Natural <: FiniteStrain end
-struct Infinitesimal <: FiniteStrain end
-
-struct StrainFromVolume{T<:FiniteStrain}
-    v0::Any
-end
-(x::StrainFromVolume{Eulerian})(v) = (cbrt(x.v0 / v)^2 - 1) / 2
-(x::StrainFromVolume{Lagrangian})(v) = (cbrt(v / x.v0)^2 - 1) / 2
-(x::StrainFromVolume{Natural})(v) = log(v / x.v0) / 3
-(x::StrainFromVolume{Infinitesimal})(v) = 1 - cbrt(x.v0 / v)
-
-struct VolumeFromStrain{T<:FiniteStrain}
-    v0::Any
-end
-(x::VolumeFromStrain{Eulerian})(f) = x.v0 / (2f + 1)^(3 / 2)
-(x::VolumeFromStrain{Lagrangian})(f) = x.v0 * (2f + 1)^(3 / 2)
-(x::VolumeFromStrain{Natural})(f) = x.v0 * exp(3f)
-(x::VolumeFromStrain{Infinitesimal})(f) = x.v0 / (1 - f)^3
+export linearfit
 
 _islocalminimum(f, x, δx) = f(x) < f(x - δx) && f(x) < f(x + δx)
 
@@ -57,13 +35,5 @@ function linearfit(volumes, energies, deg = 3)
     bs = Tuple(derivative(poly, n)(v0) / factorial(n) for n in 1:deg)
     return PolynomialEOS(v0, bs, e0)
 end # function linearfit
-
-Base.inv(x::StrainFromVolume{T}) where {T} = VolumeFromStrain{T}(x.v0)
-Base.inv(x::VolumeFromStrain{T}) where {T} = StrainFromVolume{T}(x.v0)
-
-Base.:∘(a::StrainFromVolume{T}, b::VolumeFromStrain{T}) where {T} =
-    a.v0 == b.v0 ? identity : error("operation undefined!")
-Base.:∘(a::VolumeFromStrain{T}, b::StrainFromVolume{T}) where {T} =
-    a.v0 == b.v0 ? identity : error("operation undefined!")
 
 end
